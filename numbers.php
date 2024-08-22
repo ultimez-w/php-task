18318b61b0ce5a7d471f18a2857c8627b3ba488f
ultimez-w <ultimez.developercube21@gmail.com>
2024-08-22 16:11:10 +0530

    Add files via upload

---
+++ numbers.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $a=443.3432;
+    $b=7543.829;
+    
+    
+    ?>
+    <h4><?php echo "The ceiling nunber of 443.3432 is "?><?php echo ceil($a);?></h4>
+    <h4><?php echo "The floor value of 7543.829 is"?><?php echo floor($b)?></h4>
+<h4><?php echo "The random number between 500 and 5000 is";?><?php echo (rand(5000,500));?></h4>
+</body>
+</html>
