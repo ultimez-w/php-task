708ab7cc50bea39e21ff32e472ed337e4633c9b4
ultimez-w <ultimez.developercube21@gmail.com>
2024-08-22 13:28:32 +0530

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,15 @@
+<?php 
+$College_name="Jain college of engineering";
+$Reg_no="026";
+$Branch_name="EC";
+
+ echo $College_name;
+echo $Reg_no;
+echo $Branch_name;
+
+define('Phone_number','8431955752');
+define('Deptartment','EC');
+
+echo Phone_number;
+echo Deptartment;
+?>
