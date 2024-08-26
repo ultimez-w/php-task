281f078659b977e96ea50be1379471e2110836de
ultimez-w <ultimez.developercube21@gmail.com>
2024-08-26 10:09:43 +0530

    Add files via upload

---
+++ function.php
@@ -0,0 +1,18 @@
+<?php 
+function mul($a,$b){
+     echo $a+$b; echo '<br>';
+     echo $a*$b;
+
+}
+for($i=0;$i<5;$i++){
+    echo '<br>';
+     mul(2,3);
+
+}
+for ($j=0;$j<5;$j++){
+    echo '<br>';
+     mul(2,1);
+}
+ 
+?>
+
